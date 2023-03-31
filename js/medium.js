f1f16ad527483f8a57089d76785af88385712495
Malik <malikfelix50@gmail.com>
2023-03-30 21:44:24 -0500

    WIP add medium API content

---
+++ js/medium.js
@@ -0,0 +1,27 @@
+import MEDIUM_API_KEY from "../config";
+console.log(`Your API Key is ${API_KEY}`);
+
+const fetch = require("node-fetch");
+
+// const baseURL = "https://medium2.p.rapidapi.com/article";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-KEY": MEDIUM_API_KEY,
+    "X-RapidAPI-Host": "medium2.p.rapidapi.com",
+  },
+};
+
+fetch("https://medium2.p.rapidapi.com/", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+// const mediumUrl = "https://medium2.p.rapidapi.com/article/6e419b5cf658/content";
+
+// fetch(mediumUrl)
+// .then(response => response.json())
+// .then(data => {
+//   data.result
+// })
